3197f10d5db7b6d45adb0a8c70d196560943f3ff
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2018-07-09 10:25:24 +0530

    RequirementChange: List of Cats

--- changingExpectations/catClicker.js
+++ changingExpectations/catClicker.js
@@ -11,13 +11,13 @@ function incrementCountTwo () {
 
 console.log('loaded')
 document.getElementsByClassName('cat-image-one')[0].onclick = () => {
-    console.log('clicked')
-    incrementCountOne()
-    document.getElementsByClassName('cat-count-one')[0].innerHTML = catClickedCountOne
+    document.getElementsByClassName('cat-count')[0].innerHTML = "<img src='./cat.jpg' class='cat-image-one'/>"
 }
 
 document.getElementsByClassName('cat-image-two')[0].onclick = () => {
-    console.log('clicked')
-    incrementCountTwo()
-    document.getElementsByClassName('cat-count-two')[0].innerHTML = catClickedCountTwo
+    document.getElementsByClassName('cat-count')[0].innerHTML = "<img src='./cat2.jpg' class='cat-image-two'/>"
+}
+
+document.getElementsByClassName('cat-image-three')[0].onclick = () => {
+    document.getElementsByClassName('cat-count')[0].innerHTML = "<img src='./cat3.jpg' class='cat-image-three'/>"
 }
